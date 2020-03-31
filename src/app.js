78583e3443e5e15114e62a136cdfce1d5d08456e
Santiago Carrasco <jsanticc@hotmail.com>
2020-03-31 11:45:49 -0700

    Separate app & index

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { GraphQLServer } from 'graphql-yoga';
+import path from 'path';
+
+import { resolvers } from './graphql/resolvers';
+
+const server = new GraphQLServer({
+	resolvers,
+	typeDefs: path.join(__dirname, 'graphql/schema.graphql'),
+});
+
+export default server;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import './database';
-import { GraphQLServer } from 'graphql-yoga';
-import path from 'path';
-
 import { config } from './config';
-import { resolvers } from './graphql/resolvers';
+import server from './app';
 
 const {
 	APP: { PORT },
 } = config;
 
-const server = new GraphQLServer({
-	resolvers,
-	typeDefs: path.join(__dirname, 'graphql/schema.graphql'),
-});
-
-const configGraphql = { port: PORT, endpoint: '/api' };
-
-server.start(configGraphql, ({ port }) => {
+server.start({ port: PORT, endpoint: '/api' }, ({ port }) => {
 	console.log(`Server on: http://localhost:${port}`);
 });
